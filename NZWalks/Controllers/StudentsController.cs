acf554e133c902e75394f8de2f3586a13cdf6555
shubham070 <shubhamg070@gmail.com>
2024-09-01 13:56:16 +0530

    controller class and domain classes

---
+++ NZWalks/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.Controllers
+{
+    // https://localhost:portnumbrt/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "A", "B", "C", "D" };
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ NZWalks/Models/Domain/Difficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace NZWalks.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
